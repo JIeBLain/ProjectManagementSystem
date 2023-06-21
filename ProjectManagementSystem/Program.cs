e7f7fd24a7aee110606f14a7ba40c00212209274
Lev <lev-sidorov@yandex.ru>
2023-06-21 19:10:40 +0700

    Add CORS and IIS configuration methods and the others.

--- ProjectManagementSystem/Program.cs
+++ ProjectManagementSystem/Program.cs
@@ -1,14 +1,27 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using ProjectManagementSystem.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
