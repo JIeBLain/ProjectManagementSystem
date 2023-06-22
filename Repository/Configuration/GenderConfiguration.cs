d445fe444581b6f3fc33c767aa06c0d617c32a00
Lev <lev-sidorov@yandex.ru>
2023-06-22 22:48:10 +0700

    Add Repository.csproj

---
+++ Repository/Configuration/GenderConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration;
+
+public class GenderConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.Property(p => p.Gender)
+            .HasConversion(c => c.ToString(), c => Enum.Parse<Gender>(c))
+            .HasMaxLength(10);
+    }
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Company> Companies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new GenderConfiguration());
+    }
+}
