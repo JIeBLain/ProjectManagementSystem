86552533c9d0dd452a5801989ba26a96eb78704e
Lev <lev-sidorov@yandex.ru>
2023-06-30 14:26:58 +0700

    Add nullability for DbSet

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -11,9 +11,9 @@ public class RepositoryContext : DbContext
 
     }
 
-    public DbSet<Project> Projects { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<ProjectEmployees> ProjectEmployees { get; set; }
+    public DbSet<Project>? Projects { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+    public DbSet<ProjectEmployees>? ProjectEmployees { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
