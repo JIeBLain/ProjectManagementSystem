1a7edf15b4f7f12c3936284e9003cc08cb436bc7
Lev <lev-sidorov@yandex.ru>
2023-08-21 17:31:34 +0700

    Add migration IndexingFullNameForEmployee

---
+++ ProjectManagementSystem/Migrations/20230821102539_IndexingFullNameForEmployee.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndexingFullNameForEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_LastName_FirstName_PatronymicName",
+                table: "Employees",
+                columns: new[] { "LastName", "FirstName", "PatronymicName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_LastName_FirstName_PatronymicName",
+                table: "Employees");
+        }
+    }
+}
